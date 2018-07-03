804b60d994c9b5b1e05dd782e81cdf566ff28c9b
Prabhu92 <sprabhjot14@gmail.com>
2018-07-04 00:01:41 +0530

    last night work

--- src/main/java/BotLogic.java
+++ src/main/java/BotLogic.java
@@ -5,8 +5,11 @@ import com.webcerebrium.binance.api.BinanceApi;
 import com.webcerebrium.binance.api.BinanceApiException;
 import com.webcerebrium.binance.datatype.BinanceEventDepthUpdate;
 import com.webcerebrium.binance.datatype.BinanceSymbol;
+import com.webcerebrium.binance.datatype.BinanceEventAggTrade;
 import com.webcerebrium.binance.websocket.BinanceWebSocketAdapterDepth;
+import com.webcerebrium.binance.websocket.BinanceWebSocketAdapterAggTrades;
 import org.eclipse.jetty.websocket.api.Session;
+import redis.clients.jedis.Jedis;
 
 public class BotLogic {
 
@@ -22,6 +25,8 @@ public class BotLogic {
             System.out.println( "ERROR: " + e.getMessage());
         }*/
 
+        Jedis jedis = new Jedis("localhost");
+
         try {
 
             BinanceSymbol symbol = new BinanceSymbol("ETHBTC");
@@ -29,12 +34,17 @@ public class BotLogic {
                 @Override
                 public void onMessage(BinanceEventDepthUpdate message) {
                     //System.out.println(message.getClass().getName());
+                    String symb = message.getSymbol().toString();
                     for (int i = 0; i < message.getBids().size(); i++) {
-                        System.out.println(message.getBids().get(i).getType());
+
+                        // System.out.println(message.getBids().get(i).getPrice()+ "   " + message.getBids().get(i).getQuantity());
                     }
+
+
                     for (int i = 0; i < message.getAsks().size(); i++) {
-                        System.out.println(message.getAsks().get(i));
+                        //System.out.println(message.getAsks().get(i).getPrice()+ "    "+ message.getAsks().get(i).getQuantity());
                     }
+
                 }
             });
             try {
@@ -44,6 +54,25 @@ public class BotLogic {
             session.close();
             }catch (BinanceApiException e) {
             System.out.println("ERROR: " + e.getMessage());
+            }
+
+
+            try {
+                BinanceSymbol symbol = new BinanceSymbol("ETHBTC");
+
+                Session session = (new BinanceApi()).websocketTrades(symbol, new BinanceWebSocketAdapterAggTrades() {
+                    @Override
+                    public void onMessage(BinanceEventAggTrade message) {
+                        System.out.println(message.getPrice()+"     "+ message.getQuantity());
+                    }
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                session.close();
+            }catch (BinanceApiException e) {
+                System.out.println("ERROR: " + e.getMessage());
             }
     }
 }
